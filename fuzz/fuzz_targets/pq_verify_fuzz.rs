1732df4e3d6aa7f1d7903fdc3a6d478883b1ddb8
Eternally-Thriving-Grandmasterism <ceo@acitygames.com>
2026-01-09 03:23:51 -0500

    Create pq_verify_fuzz.rs

---
+++ fuzz/fuzz_targets/pq_verify_fuzz.rs
@@ -0,0 +1,22 @@
+#![no_main]
+use libfuzzer_sys::fuzz_target;
+use arbitrary::{Arbitrary, Unstructured};
+use mercyshieldplus::pq_verify_data;
+
+#[derive(Arbitrary, Debug)]
+struct VerifyInput {
+    dsa_pk_b64: String,
+    message: Vec<u8>,
+    signature_b64: String,
+}
+
+fuzz_target!(|input: &[u8]| {
+    let mut u = Unstructured::new(input);
+    if let Ok(verify_input) = VerifyInput::arbitrary(&mut u) {
+        let _ = pq_verify_data(
+            verify_input.dsa_pk_b64,
+            verify_input.message,
+            verify_input.signature_b64,
+        );
+    }
+});
