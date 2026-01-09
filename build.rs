c8bdc0cb007c736e2ee3953b0a2bf00f285087b7
Eternally-Thriving-Grandmasterism <ceo@acitygames.com>
2026-01-09 01:42:31 -0500

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,3 +1,3 @@
 fn main() {
-    uniffi::generate_scaffolding("./src/lib.rs").unwrap();
+    uniffi::generate_scaffolding("./src/lib.rs").expect("uniFFI scaffolding generation failed — thunder mercy");
 }
